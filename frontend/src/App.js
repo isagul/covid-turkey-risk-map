e52d4c1c0788205f4779bcb9cf4b544310e75e33
isagul <isagul@Isa-MacBook-Pro.local>
2021-04-19 09:30:18 +0300

    change api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [infoDrawerVisible, setInfoDrawerVisible] = useState(false);
 
-  const apiPrefix = "https://covid-turkey-case-ratio.herokuapp.com"
+  const apiPrefix = "https://api-covid-turkey.herokuapp.com"
 
   const topographyURL =
     "https://gist.githubusercontent.com/isagul/2887858e1c759e006e604032b0e31c79/raw/438b8a8d419a5059c07ea5115aa65ca7b3f294cd/turkey.topo.json";
